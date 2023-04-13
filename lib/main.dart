fd9dfffb2f056c0d7b2a6472b2074f5fe26cf9e5
nabenduPixel <124568779+nabenduPixel@users.noreply.github.com>
2023-04-13 12:28:15 +0530

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -33,18 +35,286 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
+    var arrNames = [
+      'Sudeshna',
+      'Nabendu',
+      'Sourabh',
+      'Umakanta',
+      'Dipanwita',
+      'Sudeshna',
+      'Nabendu',
+      'Sourabh',
+      'Umakanta',
+      'Dipanwita'
+    ];
+
     return Scaffold(
         appBar: AppBar(
           title: Text('Flutter Container'),
         ),
-        body:
-            // ---------------------------------------------------------------
-            OutlinedButton(
-          child: Text('data'),
-          onPressed: () {
-            print('object');
-          },
-        )
+        body: Text('data')
+
+        //  ListView.separated(
+        //   itemBuilder: (context, index) {
+        //     return Row(
+        //       children: [
+        //         Container(
+        //           width: 200,
+        //           height: 100,
+        //           color: Colors.amber,
+        //           child: Text(
+        //             arrNames[index],
+        //             style: TextStyle(
+        //               fontSize: 21,
+        //               fontWeight: FontWeight.w500,
+        //             ),
+        //           ),
+        //         )
+        //       ],
+        //     );
+        //   },
+        //   itemCount: arrNames.length,
+        //   separatorBuilder: (context, index) {
+        //     return Divider(
+        //       height: 100,
+        //       thickness: 4,
+        //     );
+        //   },
+        //   // scrollDirection: Axis.vertical,
+        // )
+        //  ListView(
+        //   // scrollDirection: Axis.horizontal,
+        //   // reverse: true,
+        //   children: [
+        //     Padding(
+        //       padding: const EdgeInsets.all(8.0),
+        //       child: Text(
+        //         'One',
+        //         style: TextStyle(fontSize: 21, fontWeight: FontWeight.w500),
+        //       ),
+        //     ),
+        //     Padding(
+        //       padding: const EdgeInsets.all(8.0),
+        //       child: Text(
+        //         'Two',
+        //         style: TextStyle(fontSize: 21, fontWeight: FontWeight.w500),
+        //       ),
+        //     ),
+        //     Padding(
+        //       padding: const EdgeInsets.all(8.0),
+        //       child: Text(
+        //         'Three',
+        //         style: TextStyle(fontSize: 21, fontWeight: FontWeight.w500),
+        //       ),
+        //     ),
+        //     Padding(
+        //       padding: const EdgeInsets.all(8.0),
+        //       child: Text(
+        //         'Four',
+        //         style: TextStyle(fontSize: 21, fontWeight: FontWeight.w500),
+        //       ),
+        //     ),
+        //     Padding(
+        //       padding: const EdgeInsets.all(8.0),
+        //       child: Text(
+        //         'Five',
+        //         style: TextStyle(fontSize: 21, fontWeight: FontWeight.w500),
+        //       ),
+        //     ),
+        //     Padding(
+        //       padding: const EdgeInsets.all(8.0),
+        //       child: Text(
+        //         'Six',
+        //         style: TextStyle(fontSize: 21, fontWeight: FontWeight.w500),
+        //       ),
+        //     ),
+        //     Padding(
+        //       padding: const EdgeInsets.all(8.0),
+        //       child: Text(
+        //         'Seven',
+        //         style: TextStyle(fontSize: 21, fontWeight: FontWeight.w500),
+        //       ),
+        //     ),
+        //   ],
+        // )
+        // ---------------------------------------------------------------
+        // Padding(
+        //   padding: const EdgeInsets.all(8.0),
+        //   child: SingleChildScrollView(
+        //     child: Column(
+        //       children: [
+        //         Padding(
+        //           padding: const EdgeInsets.only(bottom: 11),
+        //           child: SingleChildScrollView(
+        //             scrollDirection: Axis.horizontal,
+        //             child: Row(
+        //               children: [
+        //                 Container(
+        //                   margin: EdgeInsets.only(right: 11),
+        //                   height: 200,
+        //                   width: 200,
+        //                   color: Colors.amber,
+        //                 ),
+        //                 Container(
+        //                   margin: EdgeInsets.only(right: 11),
+        //                   height: 200,
+        //                   width: 200,
+        //                   color: Colors.amber,
+        //                 ),
+        //                 Container(
+        //                   margin: EdgeInsets.only(right: 11),
+        //                   height: 200,
+        //                   width: 200,
+        //                   color: Colors.amber,
+        //                 ),
+        //                 Container(
+        //                   margin: EdgeInsets.only(right: 11),
+        //                   height: 200,
+        //                   width: 200,
+        //                   color: Colors.amber,
+        //                 ),
+        //                 Container(
+        //                   margin: EdgeInsets.only(right: 11),
+        //                   height: 200,
+        //                   width: 200,
+        //                   color: Colors.amber,
+        //                 ),
+        //                 Container(
+        //                   margin: EdgeInsets.only(right: 11),
+        //                   height: 200,
+        //                   width: 200,
+        //                   color: Colors.amber,
+        //                 ),
+        //               ],
+        //             ),
+        //           ),
+        //         ),
+        //         Container(
+        //           padding: EdgeInsets.only(bottom: 11),
+        //           height: 200,
+        //           color: Color.fromARGB(255, 247, 52, 3),
+        //         ),
+        //         Container(
+        //           padding: EdgeInsets.only(bottom: 11),
+        //           height: 200,
+        //           color: Color.fromARGB(255, 166, 213, 13),
+        //         ),
+        //         Container(
+        //           padding: EdgeInsets.only(bottom: 11),
+        //           height: 200,
+        //           color: Color.fromARGB(255, 11, 243, 11),
+        //         ),
+        //         Container(
+        //           padding: EdgeInsets.only(bottom: 11),
+        //           height: 200,
+        //           color: Color.fromARGB(255, 76, 183, 228),
+        //         ),
+        //         Container(
+        //           padding: EdgeInsets.only(bottom: 11),
+        //           height: 200,
+        //           color: Color.fromARGB(255, 255, 7, 164),
+        //         ),
+        //         Container(
+        //           padding: EdgeInsets.only(bottom: 11),
+        //           height: 200,
+        //           color: Color.fromARGB(255, 40, 7, 255),
+        //         ),
+        //         Container(
+        //           padding: EdgeInsets.only(bottom: 11),
+        //           height: 200,
+        //           color: Colors.amber,
+        //         ),
+        //       ],
+        //     ),
+        //   ),
+        // )
+        // ---------------------------------------------------------
+        // Center(
+        //     child: InkWell(
+        //   onTap: () {
+        //     print('Tapped on Container');
+        //   },
+        //   onLongPress: () {
+        //     print('Tapped Long press Button');
+        //   },
+        //   onDoubleTap: () {
+        //     print('Tapped Double Tapped');
+        //   },
+        //   child: Container(
+        //     width: 200,
+        //     height: 200,
+        //     color: Colors.amber,
+        //   ),
+        //  )
+        // )
+        // ---------------------------------------------------------
+        // Container(
+        //   height: 300,
+        //   width: 300,
+        //   child: Column(
+        //     // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        //     // mainAxisAlignment: MainAxisAlignment.spaceAround,
+        //     // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        //     // mainAxisAlignment: MainAxisAlignment.center,
+        //     // mainAxisAlignment: MainAxisAlignment.end,
+        //     mainAxisAlignment: MainAxisAlignment.start,
+        //     crossAxisAlignment: CrossAxisAlignment.start,
+        //     children: [
+        //       Row(
+        //         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        //         // crossAxisAlignment: CrossAxisAlignment.stretch,
+        //         children: [
+        //           Text(
+        //             'R1',
+        //             style: TextStyle(fontSize: 25),
+        //           ),
+        //           Text('R2', style: TextStyle(fontSize: 25)),
+        //           Text('R3', style: TextStyle(fontSize: 25)),
+        //           Text('R4', style: TextStyle(fontSize: 25)),
+        //           Text('R5', style: TextStyle(fontSize: 25)),
+        //           Text('R6', style: TextStyle(fontSize: 25)),
+        //           ElevatedButton(
+        //             onPressed: () {
+        //               print('object');
+        //             },
+        //             child: Text('Click'),
+        //           )
+        //         ],
+        //       ),
+        //       Text(
+        //         'A',
+        //         style: TextStyle(fontSize: 25),
+        //       ),
+        //       Text('B', style: TextStyle(fontSize: 25)),
+        //       Text('C', style: TextStyle(fontSize: 25)),
+        //       Text('D', style: TextStyle(fontSize: 25)),
+        //       Text('E', style: TextStyle(fontSize: 25)),
+        //       Text('F', style: TextStyle(fontSize: 25)),
+        //       ElevatedButton(
+        //         onPressed: () {
+        //           print('object');
+        //         },
+        //         child: Text('Click'),
+        //       )
+        //     ],
+        //   ),
+        // )
+
+        // ------------------------------------------------------------
+        // Center(
+        //     child: Container(
+        //   width: 100,
+        //   height: 100,
+        //   child: Image.asset('assets/images/a.jpg'),
+        // ))
+
+        // ---------------------------------------------------------------
+        //     OutlinedButton(
+        //   child: Text('data'),
+        //   onPressed: () {
+        //     print('object');
+        //   },
+        // )
         // ------------------------------------------------------------------
         // ElevatedButton(
         //   child: Text('Elevated Button'),
